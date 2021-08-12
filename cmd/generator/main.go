048c4f4596d48729a264f2c8bc4c0b442e0da99b
Muvaffak Onus <me@muvaf.com>
2021-08-12 16:59:11 +0300

    first iteration of the code generator.
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -16,8 +16,65 @@ limitations under the License.
 
 package main
 
-import "fmt"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/iancoleman/strcase"
+
+	"github.com/crossplane-contrib/terrajet/pkg/pipeline"
+	"github.com/pkg/errors"
+	"github.com/terraform-providers/terraform-provider-aws/aws"
+)
+
+const (
+	ModulePath  = "github.com/crossplane-contrib/provider-tf-aws"
+	GroupSuffix = ".aws.tf.crossplane.io"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(errors.Wrap(err, "cannot get working directory"))
+	}
+	groups := map[string]map[string]*schema.Resource{}
+	for name, resource := range aws.Provider().ResourcesMap {
+		if len(resource.Schema) == 0 {
+			// There are resources with no schema, like aws_securityhub_account
+			// that we will address later.
+			continue
+		}
+		// Both aws_config_configuration_recorder_status and aws_config_configuration_recorder
+		// exist as resource which makes kubebuilder confuse them as same resource
+		// and expect a storage version.
+		if name == "aws_config_configuration_recorder_status" {
+			continue
+		}
+		words := strings.Split(name, "_")
+		groupName := words[1]
+		if len(groups[groupName]) == 0 {
+			groups[groupName] = map[string]*schema.Resource{}
+		}
+		groups[groupName][name] = resource
+	}
+
+	for group, resources := range groups {
+		version := "v1alpha1"
+		groupGen := pipeline.NewVersionGenerator(wd, strings.ToLower(group)+GroupSuffix, version)
+		if err := groupGen.Generate(); err != nil {
+			panic(errors.Wrap(err, "cannot generate version files"))
+		}
+		groupDir := filepath.Join(wd, "apis", group)
+		crdGen := pipeline.NewCRDGenerator(groupDir, ModulePath, strings.ToLower(group)+GroupSuffix)
+		for name, resource := range resources {
+			// We don't want Aws prefix in all kinds.
+			kind := strings.TrimPrefix(strcase.ToCamel(name), "Aws")
+			if err := crdGen.Generate(version, kind, resource); err != nil {
+				panic(errors.Wrap(err, "cannot generate crd"))
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,15 +1,20 @@
-module github.com/crossplane/provider-tf-aws
+module github.com/crossplane-contrib/provider-tf-aws
 
 go 1.16
 
 require (
+	github.com/crossplane-contrib/terrajet v0.0.0-20210819124559-48f7e68db859
 	github.com/crossplane/crossplane-runtime v0.14.1-0.20210805220729-047d9387efbb
 	github.com/crossplane/crossplane-tools v0.0.0-20210320162312-1baca298c527
-	github.com/google/go-cmp v0.5.5
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
+	github.com/iancoleman/strcase v0.2.0
 	github.com/pkg/errors v0.9.1
+	github.com/terraform-providers/terraform-provider-aws v1.60.1-0.20210811232925-d6f99829ec3f
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	k8s.io/apimachinery v0.21.2
-	k8s.io/client-go v0.21.2
+	k8s.io/apimachinery v0.22.0
+	k8s.io/client-go v0.22.0
 	sigs.k8s.io/controller-runtime v0.9.2
-	sigs.k8s.io/controller-tools v0.3.0
+	sigs.k8s.io/controller-tools v0.4.0
 )
+
+replace github.com/hashicorp/terraform-plugin-sdk/v2 => github.com/gdavison/terraform-plugin-sdk/v2 v2.0.2-0.20210714181518-b5a3dc95a675
