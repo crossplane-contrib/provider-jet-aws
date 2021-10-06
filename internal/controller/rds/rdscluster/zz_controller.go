aec20f3065ff0cff7ae77470087b38df47dc63b7
Muvaffak Onus <me@muvaf.com>
2021-10-06 15:23:34 +0300

    rds: enable async mode and add example
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- internal/controller/rds/rdscluster/zz_controller.go
+++ internal/controller/rds/rdscluster/zz_controller.go
@@ -41,12 +41,13 @@ func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, s terra
 	name := managed.ControllerName(v1alpha1.RdsClusterGroupVersionKind.String())
 	r := managed.NewReconciler(mgr,
 		xpresource.ManagedKind(v1alpha1.RdsClusterGroupVersionKind),
-		managed.WithExternalConnecter(tjcontroller.NewConnector(mgr.GetClient(), ws, s)),
+		managed.WithExternalConnecter(tjcontroller.NewConnector(mgr.GetClient(), ws, s,
+			tjcontroller.UseAsync(),
+		)),
 		managed.WithLogger(l.WithValues("controller", name)),
 		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))),
 		managed.WithFinalizer(terraform.NewWorkspaceFinalizer(ws, xpresource.NewAPIFinalizer(mgr.GetClient(), managed.FinalizerName))),
 		managed.WithTimeout(3*time.Minute),
-		managed.WithInitializers(),
 	)
 
 	return ctrl.NewControllerManagedBy(mgr).
