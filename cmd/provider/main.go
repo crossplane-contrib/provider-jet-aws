3ef3441b985011a3ff434cb84bac275e9ccdb542
Muvaffak Onus <me@muvaf.com>
2021-10-20 13:47:43 +0300

    terrajet: update to get faster readiness switch
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -20,15 +20,14 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/crossplane-contrib/terrajet/pkg/config"
-
-	"github.com/crossplane-contrib/terrajet/pkg/terraform"
-
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"gopkg.in/alecthomas/kingpin.v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/crossplane-contrib/terrajet/pkg/config"
+	"github.com/crossplane-contrib/terrajet/pkg/terraform"
 
 	"github.com/crossplane-contrib/provider-tf-aws/apis"
 	_ "github.com/crossplane-contrib/provider-tf-aws/config"
