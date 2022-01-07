ce2c4763431e2062c22381d4d27968421e4d2fbe
Muvaffak Onus <me@muvaf.com>
2022-01-07 12:15:19 +0300

    update go, crossplane-runtime and k8s libraries to the latest
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- internal/controller/providerconfig/config.go
+++ internal/controller/providerconfig/config.go
@@ -16,14 +16,11 @@ package providerconfig
 import (
 	"github.com/crossplane/terrajet/pkg/config"
 	"github.com/crossplane/terrajet/pkg/terraform"
-	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
+	"github.com/crossplane/crossplane-runtime/pkg/controller"
 	"github.com/crossplane/crossplane-runtime/pkg/event"
-	"github.com/crossplane/crossplane-runtime/pkg/logging"
-	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/providerconfig"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 
@@ -32,13 +29,8 @@ import (
 
 // Setup adds a controller that reconciles ProviderConfigs by accounting for
 // their current usage.
-func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, _ terraform.SetupFn, _ *terraform.WorkspaceStore, _ *config.Provider, concurrency int) error {
+func Setup(mgr ctrl.Manager, o controller.Options, _ terraform.SetupFn, _ *terraform.WorkspaceStore, _ *config.Provider) error {
 	name := providerconfig.ControllerName(v1alpha1.ProviderConfigGroupKind)
-
-	o := controller.Options{
-		RateLimiter:             ratelimiter.NewController(rl),
-		MaxConcurrentReconciles: concurrency,
-	}
 
 	of := resource.ProviderConfigKinds{
 		Config:    v1alpha1.ProviderConfigGroupVersionKind,
@@ -47,10 +39,10 @@ func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, _ terra
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
-		WithOptions(o).
+		WithOptions(o.ForControllerRuntime()).
 		For(&v1alpha1.ProviderConfig{}).
 		Watches(&source.Kind{Type: &v1alpha1.ProviderConfigUsage{}}, &resource.EnqueueRequestForProviderConfig{}).
 		Complete(providerconfig.NewReconciler(mgr, of,
-			providerconfig.WithLogger(l.WithValues("controller", name)),
+			providerconfig.WithLogger(o.Logger.WithValues("controller", name)),
 			providerconfig.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
 }
