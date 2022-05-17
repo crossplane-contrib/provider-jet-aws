7e36b9cc1ace590f3a1d15e7949be781a8d932c0
vaspahomov <vas2142553@gmail.com>
2022-05-17 14:21:21 +0500

    Add aws_nat_gateway resource
    Signed-off-by: vaspahomov <vas2142553@gmail.com>

--- config/provider.go
+++ config/provider.go
@@ -110,6 +110,7 @@ var IncludedResources = []string{
 	"aws_vpc_peering_connection$",
 	"aws_route_table_association$",
 	"aws_internet_gateway$",
+	"aws_nat_gateway$",
 
 	// IAM
 	"aws_iam_access_key$",
